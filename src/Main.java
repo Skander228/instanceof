f7d9e05afaf1a3e65908959a6a74cfdcdc2543db
Skander228 <kkhemiriskander@gmail.com>
2022-06-23 22:57:38 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,48 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+class Student{}
+class Rockstar{}
+class Hacker{}
+
+public class Main {
+    static String count(ArrayList<Object> mylist){
+        int a = 0, b = 0, c = 0;
+        for (Object element : mylist) {
+            if (element instanceof Student) {
+                a++;
+            }
+
+            if (element instanceof Rockstar) {
+                b++;
+            }
+
+            if (element instanceof Hacker) {
+                c++;
+            }
+        }
+        return (a) + " " + (b) + " " + (c);
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Object> mylist = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        for(int i = 0; i < t; i++){
+            String s = sc.next();
+
+            if(s.equals("Student")) {
+                mylist.add(new Student());
+            }
+
+            if(s.equals("Rockstar")) {
+                mylist.add(new Rockstar());
+            }
+
+            if(s.equals("Hacker")) {
+                mylist.add(new Hacker());
+            }
+        }
+        System.out.println(count(mylist));
+    }
+}
